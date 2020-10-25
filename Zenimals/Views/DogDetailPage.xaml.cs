443a55fb008f82328bb5be56b478d8cc487dfc65
Zeebs <zeebsmail@gmail.com>
2020-10-25 11:45:43 +0200

    Wire Up ZenMvvm for Dogs

--- Zenimals/Views/DogDetailPage.xaml.cs
+++ Zenimals/Views/DogDetailPage.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Linq;
-using Xamarin.Forms;
-using Zenimals.Data;
+﻿using Xamarin.Forms;
 
 namespace Zenimals.Views
 {
-    [QueryProperty("Name", "name")]
     public partial class DogDetailPage : ContentPage
     {
-        public string Name
-        {
-            set
-            {
-                BindingContext = DogData.Dogs.FirstOrDefault(m => m.Name == Uri.UnescapeDataString(value));
-            }
-        }
-
         public DogDetailPage()
         {
             InitializeComponent();
--- Zenimals/Views/DogsPage.xaml.cs
+++ Zenimals/Views/DogsPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using Xamarin.Forms;
-using Zenimals.Models;
+﻿using Xamarin.Forms;
 
 namespace Zenimals.Views
 {
@@ -9,15 +7,6 @@ namespace Zenimals.Views
         public DogsPage()
         {
             InitializeComponent();
-        }
-
-        async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string dogName = (e.CurrentSelection.FirstOrDefault() as Animal).Name;
-            // This works because route names are unique in this application.
-            await Shell.Current.GoToAsync($"dogdetails?name={dogName}");
-            // The full route is shown below.
-            // await Shell.Current.GoToAsync($"//animals/domestic/dogs/dogdetails?name={dogName}");
         }
     }
 }
