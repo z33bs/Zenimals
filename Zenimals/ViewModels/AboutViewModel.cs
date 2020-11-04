bde7d071c9b9b813745bf4c7ea8ced5cd6dc0683
Zeebs <zeebsmail@gmail.com>
2020-11-04 11:57:48 +0200

    Add AboutViewModel

---
+++ Zenimals/ViewModels/AboutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+using Xamarin.Essentials;
+using ZenMvvm.Helpers;
+
+namespace Zenimals.ViewModels
+{
+    public class AboutViewModel
+    {
+        public ICommand TapCommand
+            => new SafeCommand<string>(Launcher.OpenAsync, mustRunOnCurrentSyncContext:true);
+    }
+}
--- Zenimals/Views/AboutPage.xaml.cs
+++ Zenimals/Views/AboutPage.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System.Windows.Input;
-using Xamarin.Essentials;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Zenimals.Views
 {
     public partial class AboutPage : ContentPage
     {
-        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-
         public AboutPage()
         {
             InitializeComponent();
-            BindingContext = this;
         }
     }
 }
